59655bc530f12156f2092cee67e2beb9c065309d
Mitch Richling <11151403+richmit@users.noreply.github.com>
2025-07-29 09:50:40 -0500

    Added headers

--- src/curve_template.f90
+++ src/curve_template.f90
@@ -1,12 +1,43 @@
 ! -*- Mode:F90; Coding:us-ascii-unix; fill-column:129 -*-
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.H.S.!!
+!>
+!! @file      curve_template.f90
+!! @author    Mitch Richling http://www.mitchr.me/
+!! @brief     Template to solve Strange Attractor ODEs.@EOL
+!! @std       F2023
+!! @see       https://github.com/richmit/StrangeAttractorZoo/
+!! @copyright 
+!!  @parblock
+!!  Copyright (c) 2025, Mitchell Jay Richling <http://www.mitchr.me/> All rights reserved.
+!!  
+!!  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+!!  conditions are met:
+!!  
+!!  1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following
+!!     disclaimer.
+!!  
+!!  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following
+!!     disclaimer in the documentation and/or other materials provided with the distribution.
+!!  
+!!  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
+!!     derived from this software without specific prior written permission.
+!!  
+!!  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+!!  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+!!  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+!!  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+!!  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+!!  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+!!  OF THE POSSIBILITY OF SUCH DAMAGE.
+!!  @endparblock
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.H.E.!!
 
 !----------------------------------------------------------------------------------------------------------------------------------
 program curve_NAME
-  use, intrinsic :: iso_fortran_env,    only: output_unit, error_unit
-  use            :: mrkiss_config,      only: rk, ik, t_delta_tiny
-  use            :: mrkiss_solvers_nt,  only: steps_fixed_stab_nt
-  use            :: mrkiss_utils,       only: print_t_y_sol
-!  use            :: mrkiss_erk_kutta_4, only: a, b, c
+  use, intrinsic :: iso_fortran_env,          only: output_unit, error_unit
+  use            :: mrkiss_config,            only: rk, ik, t_delta_tiny
+  use            :: mrkiss_solvers_nt,        only: steps_fixed_stab_nt
+  use            :: mrkiss_utils,             only: print_t_y_sol
   use            :: mrkiss_eerk_fehlberg_7_8, only: a, b=>b2, c
 
   implicit none
@@ -14,7 +45,7 @@ program curve_NAME
   integer,        parameter :: deq_dim       = 3
   integer,        parameter :: num_points    = MAX_POINTS
   real(kind=rk),  parameter :: y_iv(deq_dim) = Y_IV
-  real(kind=rk),  parameter :: param(PVD)      = PARAM_VALUE
+  real(kind=rk),  parameter :: param(PVD)    = PARAM_VALUE
   real(kind=rk),  parameter :: t_delta       = T_DELTA_INI
   real(kind=rk),  parameter :: t_max         = T_MAX
   real(kind=rk),  parameter :: t_min         = T_MIN
--- src/curve_template.py
+++ src/curve_template.py
@@ -1,9 +1,40 @@
+# -*- Mode:Python; Coding:us-ascii-unix; fill-column:158 -*-
+#########################################################################################################################################################.H.S.##
+##
+# @file      curve_template.py
+# @author    Mitch Richling http://www.mitchr.me/
+# @brief     Generated glimpse and paraview startup files.@EOL
+# @std       Python
+# @see       https://github.com/richmit/StrangeAttractorZoo/
+# @copyright 
+#  @parblock
+#  Copyright (c) 2025, Mitchell Jay Richling <http://www.mitchr.me/> All rights reserved.
+#  
+#  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#  
+#  1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+#  
+#  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#  
+#  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#  
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+#  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+#  @endparblock
+#########################################################################################################################################################.H.E.##
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
 #import paraview
 #paraview.compatibility.major = 5
 #paraview.compatibility.minor = 13
 
-#### import the simple module from the paraview
+# import the simple paraview module 
 from paraview.simple import *
 
 # disable automatic camera reset on 'Show'
@@ -59,11 +90,6 @@ curveMJRDisplay.LineWidth = 10.0
 
 # Properties modified on curveMJRDisplay
 curveMJRDisplay.RenderLinesAsTubes = 1
-
-# reset view to fit data
-renderView1.ResetCamera(False, 0.9)
-
-renderView1.ApplyIsometricView()
 
 # reset view to fit data
 renderView1.ResetCamera(False, 0.9)
